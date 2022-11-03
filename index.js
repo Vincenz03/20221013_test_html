c73c35e6ecac1366e99b4d82eee43606fb53f6ce
Vincenz03 <117346448+Vincenz03@users.noreply.github.com>
2022-11-03 09:37:20 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("TEST 2022-11-03-02")
+let x = 34;
+console .log(typeof(x))
+let myString = "my firstString";
+console.log("type of string ->", typeof(myString)
+console.log("myString ->", myString)
+console.log("myString ->", myString)
+
+Let myStringDoubleApo = "my \"first string\""
+console.log("myStringDoubleApo ->", myStringDoubleApo)
+
+Let myStringBSlash = "my \"first string\""
+console.log("myStringBSlash ->", myStringBSlash)
+
+Let myStringNLine = "my \"first string\""
+console.log("myStringNLine ->", myStringNLine)
+
+Let myStringLength = "my \"first string\""
+console.log("myStringLength ->", myStringLength)
+
+
+Let myStringOrig = "my first string"
+Let myStringOrigSliced = "my first string".slice(3, 6)
+
+Let myStringOrig -- myStringOrigSliced ->", myStringOrig;
+
+
+
+
+
+
+
+
+
